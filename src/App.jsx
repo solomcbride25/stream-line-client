14ca15aee0cbfe323c50a0008c7a3156d9bf523e
solomcbride25 <solomcbride22@gmail.com>
2025-06-24 02:21:26 -0400

    frontend rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import { useEffect } from 'react'
 import './App.css'
 import Home from './components/Home'
 import Login from './components/Login'
 import Signup from './components/Signup'
 
 function App() {
-
+  useEffect(() => {
+    fetch('https://stream-line-server.onrender.com')
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.error(err));
+  }, [])
 
   return (
     <>
-    <Login/> 
-    <Signup/> 
-    <Home/>
+      <Login />
+      <Signup />
+      <Home />
     </>
   )
 }
 
-export default App
+export default App;
