bc2815a915dbaf40b907ea164d0fd7ca5563ecd6
solomcbride25 <solomcbride22@gmail.com>
2025-06-24 15:43:17 -0400

    redirect file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom' 
 import Home from './components/Home'
 import Login from './components/Login'
 import Signup from './components/Signup'
+import CSS from '.'
 
 function App() {
   useEffect(() => {
